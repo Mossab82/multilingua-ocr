58d217e1db51d2284c80f55d2f241bbf5442aa00
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2025-02-09 11:28:18 +0300

    Create __init__.py

---
+++ multilingua_ocr/evaluation/__init__.py
@@ -0,0 +1,11 @@
+from .metrics import (
+    OCRMetrics,
+    CulturalPreservationMetrics,
+    ScriptAccuracyMetrics
+)
+from .visualization import (
+    DegradationVisualizer,
+    AttentionVisualizer,
+    ScriptConfusionMatrix,
+    ResultsPlotter
+)
